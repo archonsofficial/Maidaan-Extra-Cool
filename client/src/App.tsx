9d858342301822c03914cb6b1e6c0a1723317709
Alia Haider <turabhaider786@outlook.com>
2025-03-27 23:10:01 +0530

    Foooter component creation.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,15 @@
+import Footer from "./components/Footer"
+import { BrowserRouter as Router } from "react-router-dom"
 
 
 function App() {
   return (
-    <div className="text-black">
+    <Router>
+    <div className="bg-black">
 
-      <p className="bg-amber-300">
-          Khoon mai tere mitti mitti mai tera khoon upar HP niche tpc beech mai tera sukoon.... 
-      </p>
+      <Footer/>
     </div>
+    </Router>
     
   )
 }
